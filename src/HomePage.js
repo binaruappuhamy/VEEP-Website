a8df489e5b05680856d0e4621984980bd3658028
don <40864187+binaruappuhamy@users.noreply.github.com>
2022-02-05 20:27:09 -0500

    Complete about me page

--- src/HomePage.js
+++ src/HomePage.js
@@ -17,71 +17,17 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <div className='card_list'>
-            <div className='card text-black bg-info' style={{ width: '18rem' }}>
-              <Card.Img src="animu_logo.png" alt="Logo" />
-              <Card.Body>
-                <Card.Title><p className='card_title'>About me</p></Card.Title>
-                <Card.Text>
-                  <Container>
-                    <p className='card_info_grid'>
-                      <p className='card_attribute_name red_font'>Name : </p>
-                      <p className='card_attribute_value'>Binaru Appuhamy</p>
-                    </p>
-                    <p className='card_info_grid'>
-                      <p className='card_attribute_name'>Current year : </p>
-                      <p className='card_attribute_value'>ECE PEY</p>
-                    </p>
-                    <p className='card_info_grid'>  
-                      <p className='card_attribute_name'>Hobbies : </p>
-                      <p className='card_attribute_value'>Read manga and watch anime!</p>
-                    </p>
-                  </Container>
-                </Card.Text>
-                <Accordion flush>
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>More Hobbies</Accordion.Header>
-                    <Accordion.Body>
-                      Nope nothin here.
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
-              </Card.Body>
-            </div>
-            <div className ='card text-black bg-info' style={{ width: '18rem' }}>
-              <Card.Img className="circle_crop" src="animu_logo.png" alt="Logo" />
-              <Card.Body>
-                <Card.Title><p className='card_title'>About me</p></Card.Title>
-                <Card.Text>
-                  <Container>
-                    <p className='card_info_grid'>
-                      <p className='card_attribute_name'>Name : </p>
-                      <p className='card_attribute_value'>Binaru Appuhamy</p>
-                    </p>
-                    <p className='card_info_grid'>
-                      <p className='card_attribute_name'>Current year : </p>
-                      <p className='card_attribute_value'>ECE PEY</p>
-                    </p>
-                    <p className='card_info_grid'>  
-                      <p className='card_attribute_name'>Hobbies : </p>
-                      <p className='card_attribute_value'>Read manga and watch anime!</p>
-                    </p>
-                  </Container>
-                </Card.Text>
-                <Button onClick={this.toggleshow} className="mb-2">
-                  Click me
-                </Button>
-                <Toast show={this.state.show} onClose={this.toggleshow}>
-                  <Toast.Header>
-                    <img className= "image_minify" src="animu_logo.png" alt="Logo"/>
-                    <strong className="me-auto">Binaru</strong>       
-                    <small>Just now</small>
-                  </Toast.Header>
-                  <Toast.Body>Wow you are reading this</Toast.Body>
-                </Toast>
-              </Card.Body>
-            </div>
+        <div align='center'>
+          <img src="uoft_logo.jpg" alt="UofT Logo" width="179" height="79" />
+          <h1>Welcome to VEEP Website!</h1>
+        </div>
+        <div>
+          <h2><u>About me</u></h2>
+              <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Name :</b> Binaru Appuhamy<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Current year :</b> ECE PEY<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Hobbies :</b> Read manga and watch anime!<br />
+              </p>
         </div>
       </div>
     )
