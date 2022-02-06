7900661a8e9a4c8a62d95bac77456808b27d87c2
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2022-02-05 19:25:14 -0500

    testing

--- veep/src/HomePage.js
+++ veep/src/HomePage.js
@@ -7,6 +7,7 @@ class HomePage extends React.Component {
       <div>
         {/* Complete your training in here*/}
         <div>Welcome to VEEP Website!</div>
+        <h1>testing autho</h1>
       </div>
     )
   }
